d6e7d3bb2870eb953261cd9985e694abca4c9148
hugoAssal <hugo.assal1@etu.univ-lorraine.fr>
2024-04-02 13:42:48 +0200

    Modification et ajout des adjoints plus simple

--- src/Form/ResponsableType.php
+++ src/Form/ResponsableType.php
@@ -5,9 +5,11 @@ namespace App\Form;
 use App\Entity\Departement;
 use App\Entity\Employe;
 use App\Entity\Groupes;
+
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -24,13 +26,23 @@ class ResponsableType extends AbstractType
                 'multiple' => false,
                 'expanded' => false,
             ])
-            ->add('adjoints', EntityType::class, [
-                'class' => Employe::class,
-                'choice_label' => function(Employe $employe) {
-                    return $employe->getPrenom() . ' ' . $employe->getNom();
-                },
-                'multiple' => true,
-                'expanded' => true,
+            ->add('adjoints', CollectionType::class, [
+                'entry_type' => EntityType::class,
+                'entry_options' => [
+                    'class' => Employe::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('e')
+                            ->orderBy('e.nom', 'ASC');
+                    },
+                    'choice_label' => function(Employe $employe) {
+                        return $employe->getPrenom() . ' ' . $employe->getNom();
+                    },
+                    'label' => false,
+                ],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'label' => 'Ajouter un adjoint : ',
             ])
         ;
     }
